c7e3e51ba9edd3b07da79a0245689c79ace613c7
balajie chandrasekar <balajiechandru92@gmail.com>
2022-01-28 21:48:29 +0530

    initial commit

---
+++ resttemplatepractice/src/main/java/com/klassy/resttemplatepractice/ResttemplatepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.klassy.resttemplatepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplatepracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplatepracticeApplication.class, args);
+	}
+
+}
